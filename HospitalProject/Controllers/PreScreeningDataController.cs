99a1b512d4aed448940efeb54db8f6ef831a401d
Karmugil Kalaiyarasan Kavitha <n01477746@humber.ca>
2022-04-01 07:35:13 -0400

    complete CRUD operation for the PreScreening feature except visual enhancements- commit three by karmugil

---
+++ HospitalProject/Controllers/PreScreeningDataController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using HospitalProject.Models;
+
+namespace HospitalProject.Controllers
+{
+    public class PreScreeningDataController : ApiController
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: api/PreScreeningData
+        public IQueryable<PreScreening> GetPreScreenings()
+        {
+            return db.PreScreenings;
+        }
+
+        // GET: api/PreScreeningData/5
+        [ResponseType(typeof(PreScreening))]
+        public IHttpActionResult GetPreScreening(int id)
+        {
+            PreScreening preScreening = db.PreScreenings.Find(id);
+            if (preScreening == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(preScreening);
+        }
+
+        // PUT: api/PreScreeningData/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutPreScreening(int id, PreScreening preScreening)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != preScreening.Id)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(preScreening).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PreScreeningExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/PreScreeningData/AddPreScreening
+        [ResponseType(typeof(PreScreening))]
+        [HttpPost]
+        public IHttpActionResult AddPreScreening(PreScreening preScreening)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.PreScreenings.Add(preScreening);
+            db.SaveChanges();
+
+            return CreatedAtRoute("DefaultApi", new { id = preScreening.Id }, preScreening);
+        }
+
+        // DELETE: api/PreScreeningData/5
+        [ResponseType(typeof(PreScreening))]
+        public IHttpActionResult DeletePreScreening(int id)
+        {
+            PreScreening preScreening = db.PreScreenings.Find(id);
+            if (preScreening == null)
+            {
+                return NotFound();
+            }
+
+            db.PreScreenings.Remove(preScreening);
+            db.SaveChanges();
+
+            return Ok(preScreening);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PreScreeningExists(int id)
+        {
+            return db.PreScreenings.Count(e => e.Id == id) > 0;
+        }
+    }
+}
--- HospitalProject/Models/Patient.cs
+++ HospitalProject/Models/Patient.cs
@@ -15,4 +15,14 @@ namespace HospitalProject.Models
         public string Phone { get; set; }
         public DateTime DateOfBirth { get; set; }
     }
+
+    public class PatientDto
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+
 }

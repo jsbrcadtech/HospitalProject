9e5467587ed0a58ec51a3963d6bf72f89701f68b
Pallav Dubey <pallav.dubey@gmail.com>
2022-04-22 20:12:20 -0400

    validate pre screening before adding appointment

--- HospitalProject/Controllers/AppointmentController.cs
+++ HospitalProject/Controllers/AppointmentController.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using HospitalProject.Models;
+using Microsoft.AspNet.Identity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
 
@@ -8,9 +11,27 @@ namespace HospitalProject.Controllers
 {
     public class AppointmentController : Controller
     {
+        private readonly HttpClient _client;
+
+        public AppointmentController()
+        {
+            _client = new HttpClient();
+            _client.BaseAddress = new Uri("https://localhost:44397");
+        }
+
         // GET: Appointment
         public ActionResult Add()
         {
+            var userId = User.Identity.GetUserId();
+            var res = _client.GetAsync($"/api/PreScreening/{userId}").Result;
+            var preScreening = res.Content.ReadAsAsync<PreScreening>().Result;
+
+            ViewBag.PreScreeningCompleted = true;
+            if (preScreening == null || preScreening.CreatedAt < DateTime.Now.Date)
+            {
+                ViewBag.PreScreeningCompleted = false;
+            }
+
             return View();
         }
     }
